e964fc56ff4ec074bba3791ce8d76d9d3060282b
tom <t.vitureau@purjus.fr>
2017-03-11 14:28:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# VueJS Konami code plugin
+
+Enable the [Konami code](https://en.wikipedia.org/wiki/Konami_Code) on your VueJS application.
+
+## Usage
+
+```js
+Vue.use(KonamiCode, {callback: function () {
+  alert('Snake? SNAAAAAAAAAAAAAKE')
+}})
+```
---
+++ vue-konami-code.js
@@ -0,0 +1,18 @@
+let install = function (Vue, options) {
+  const kode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  const length = kode.length
+  let pos = 0
+  document.addEventListener('keydown', function (event) {
+    if (event.keyCode === kode[pos++]) {
+      if (length === pos) {
+        options.callback()
+        pos = 0 // ability to start over
+        return false
+      }
+    } else {
+      pos = 0
+    }
+  }, false)
+}
+
+typeof module !== 'undefined' && typeof module.exports !== 'undefined' && (module.exports = install)
