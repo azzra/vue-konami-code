c8b21ee0479b3cda6b6c66d30db3778c00d0db72
tom <t.vitureau@purjus.fr>
2017-03-12 10:58:20 +0100

    tests

--- src/vue-konami-code.js
+++ src/vue-konami-code.js
@@ -1,7 +1,7 @@
-let install = function (Vue, options) {
+var install = function (Vue, options) {
   const kode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
   const length = kode.length
-  let pos = 0
+  var pos = 0
   document.addEventListener('keydown', function (event) {
     if (event.keyCode === kode[pos++]) {
       if (length === pos) {
